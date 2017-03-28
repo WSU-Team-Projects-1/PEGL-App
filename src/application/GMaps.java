8950e45db8346992ca5e381d6fac78e5ea395fc2
Corey Miller <coreymiller1994@hotmail.com>
2017-03-28 01:47:50 -0400

    Gmaps.java can now get bounds of map, references #4
    Intial methods required to grab all 4 LatLng coordinates
    of the static google map are implimented with the accuracy
    that is required for this project.
    
    Next step is to use these bounds to plot any LatLng
    coordinates on the static map using the associations between
    these points and the width and height in pixels of the image.
    This will allow us to draw them at the proper pixel coordinates.

--- src/application/GMaps.java
+++ src/application/GMaps.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.net.URL;
-
+  
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,22 +12,83 @@ import com.google.maps.model.LatLng;
 public class GMaps {
 
 	public static void main(String[] args) throws Exception {
-		
-		LatLng point1 = new LatLng(39.781225, -84.064250);
-	    
+		LatLng point1 = new LatLng(39.781, -84.064);
+		LatLng cornerPoint1 = new LatLng(0, 0);
+		cornerPoint1 = getBottomLeftBoundLatLng( getXProj(point1, 480), getYProj(point1, 480, 480), 480, 480, 16); 
+	    int xbound = 480;
+	    int ybound = 480;
 	    JFrame frame = new JFrame("Test Map");
-	    URL imageUrl = new URL("http://maps.googleapis.com/maps/api/staticmap?zoom=18&"
-	    		+ "size=600x600&markers=" + point1.lat + "," + point1.lng + "&sensor=true");
+	    URL imageUrl = new URL("http://maps.googleapis.com/maps/api/staticmap?zoom=16&"
+	    		+ "size=" + xbound + "x" + ybound + "&center=" + point1.lat + "," + point1.lng + "&sensor=true");
 	    frame.add(new JLabel(new ImageIcon(new ImageIcon(imageUrl).getImage())));
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(600, 600);
+	    frame.setSize(480, 480);
 	    frame.setVisible(true);
 	}
 	
-	public static String cleanHTML(String s) {
-		while (s.contains("<")) {
-			s = s.substring(0, s.indexOf("<")) + s.substring(s.indexOf(">")+1, s.length());
-		}
-		return s;
+	public static LatLng getTopRightBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
+		LatLng point = new LatLng(0, 0);
+		xProj = xProj + (.000016 * xProj);
+		yProj = yProj + (.000003 * yProj);
+		xProj = ((xProj * Math.pow(2, zoom)) + (mapWidth / 2)) / Math.pow(2, zoom);
+		yProj = ((yProj * Math.pow(2, zoom)) - (mapHeight / 2)) / Math.pow(2, zoom); 
+		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
+		return point;	
+	} 
+	
+	public static LatLng getTopLeftBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
+		LatLng point = new LatLng(0, 0);
+		xProj = xProj - (.000016 * xProj);
+		yProj = yProj + (.000003 * yProj);
+		xProj = ((xProj * Math.pow(2, zoom)) - (mapWidth / 2)) / Math.pow(2, zoom);
+		yProj = ((yProj * Math.pow(2, zoom)) - (mapHeight / 2)) / Math.pow(2, zoom); 
+		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
+		return point;	
 	}
+	
+	public static LatLng getBottomLeftBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
+		LatLng point = new LatLng(0, 0);
+		xProj = xProj - (.000011 * xProj);
+		yProj = yProj - (.0000045 * yProj);
+		xProj = ((xProj * Math.pow(2, zoom)) - (mapWidth / 2)) / Math.pow(2, zoom);
+		yProj = ((yProj * Math.pow(2, zoom)) + (mapHeight / 2)) / Math.pow(2, zoom); 
+		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
+		return point;	
+	} 
+	
+	public static LatLng getBottomRightBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
+		LatLng point = new LatLng(0, 0);
+		xProj = xProj + (.000011 * xProj);
+		yProj = yProj - (.0000045 * yProj);
+		xProj = ((xProj * Math.pow(2, zoom)) + (mapWidth / 2)) / Math.pow(2, zoom);
+		yProj = ((yProj * Math.pow(2, zoom)) + (mapHeight / 2)) / Math.pow(2, zoom); 
+		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
+		return point;	
+	} 
+	
+	
+	public static double getXProj(LatLng point, int mapWidth){
+		double x = 0.0; 
+		x = (point.lng + 180) * (mapWidth/ 360);
+		return x;
+	}
+	
+	public static double getYProj(LatLng point, int mapWidth, int mapHeight){
+		double y = 0.0;
+		double mercN = 0.0;
+		y = point.lat * Math.PI / 180;
+		mercN = Math.log10(Math.tan((Math.PI/4) + (y/2)));
+		y = (mapHeight/2) - (mapWidth * mercN / (2 * Math.PI));
+		return y;
+	}
+	
+	public static LatLng convertProjToLatLng(double xProj, double yProj, int mapWidth, int mapHeight){
+		double latCoordinate = 0.0;
+		double longCoordinate = 0.0;
+		longCoordinate = (xProj / (mapWidth/360)) - 180;
+		latCoordinate = (((Math.atan((Math.pow(10, (Math.PI * ((((-2.0 * yProj) + mapHeight)) / mapWidth))))) - ( Math.PI / 4)) * 360) / Math.PI);	
+		LatLng point = new LatLng(latCoordinate, longCoordinate);	
+		return point;
+	}
+	
 }
