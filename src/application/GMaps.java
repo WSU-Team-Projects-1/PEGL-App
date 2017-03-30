4557d232c64475c5463ec2d8e74361a05f53673c
Corey Miller <coreymiller1994@hotmail.com>
2017-03-30 08:55:33 -0400

    Final tweaks to corner bound markers
    Final adjustments to corner bound projections for gmaps
    to be more precise. Deleted unused GMapsFX.html file.

--- src/application/GMaps.java
+++ src/application/GMaps.java
@@ -12,24 +12,34 @@ import com.google.maps.model.LatLng;
 public class GMaps {
 
 	public static void main(String[] args) throws Exception {
-		LatLng point1 = new LatLng(39.781, -84.064);
-		LatLng cornerPoint1 = new LatLng(0, 0);
-		cornerPoint1 = getBottomLeftBoundLatLng( getXProj(point1, 480), getYProj(point1, 480, 480), 480, 480, 16); 
-	    int xbound = 480;
-	    int ybound = 480;
+		LatLng point1 = new LatLng(39.739795, -84.181692);
+		LatLng cornerPoint1BR = new LatLng(0, 0);
+		LatLng cornerPoint1BL = new LatLng(0, 0);
+		LatLng cornerPoint1TR = new LatLng(0, 0);
+		LatLng cornerPoint1TL = new LatLng(0, 0);
+		int pixelWidth = 700;
+		int pixelHeight = 700;
+		cornerPoint1BR = getBottomRightBoundLatLng( getXProj(point1, pixelWidth), getYProj(point1, pixelWidth, pixelHeight), pixelWidth, pixelHeight, 16); 
+		cornerPoint1BL = getBottomLeftBoundLatLng( getXProj(point1, pixelWidth), getYProj(point1, pixelWidth, pixelHeight), pixelWidth, pixelHeight, 16);
+		cornerPoint1TR = getTopRightBoundLatLng( getXProj(point1, pixelWidth), getYProj(point1, pixelWidth, pixelHeight), pixelWidth, pixelHeight, 16);
+		cornerPoint1TL = getTopLeftBoundLatLng( getXProj(point1, pixelWidth), getYProj(point1, pixelWidth, pixelHeight), pixelWidth, pixelHeight, 16);
 	    JFrame frame = new JFrame("Test Map");
 	    URL imageUrl = new URL("http://maps.googleapis.com/maps/api/staticmap?zoom=16&"
-	    		+ "size=" + xbound + "x" + ybound + "&center=" + point1.lat + "," + point1.lng + "&sensor=true");
+	    		+ "size=" + pixelWidth + "x" + pixelHeight + "&center=" + point1.lat + "," + point1.lng + "&markers=" + cornerPoint1BR.lat + "," + cornerPoint1BR.lng 
+	    		+ "|" + cornerPoint1BL.lat + "," + cornerPoint1BL.lng + 
+	    		"|" + cornerPoint1TR.lat + "," + cornerPoint1TR.lng +
+	    		"|" + cornerPoint1TL.lat + "," + cornerPoint1TL.lng + "&sensor=true");
 	    frame.add(new JLabel(new ImageIcon(new ImageIcon(imageUrl).getImage())));
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(480, 480);
+	    frame.setSize(pixelWidth, pixelHeight);
 	    frame.setVisible(true);
 	}
+	
 	
 	public static LatLng getTopRightBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
 		LatLng point = new LatLng(0, 0);
 		xProj = xProj + (.000016 * xProj);
-		yProj = yProj + (.000003 * yProj);
+		yProj = yProj - (.0000013 * yProj);
 		xProj = ((xProj * Math.pow(2, zoom)) + (mapWidth / 2)) / Math.pow(2, zoom);
 		yProj = ((yProj * Math.pow(2, zoom)) - (mapHeight / 2)) / Math.pow(2, zoom); 
 		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
@@ -38,8 +48,8 @@ public class GMaps {
 	
 	public static LatLng getTopLeftBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
 		LatLng point = new LatLng(0, 0);
-		xProj = xProj - (.000016 * xProj);
-		yProj = yProj + (.000003 * yProj);
+		xProj = xProj - (.0000155 * xProj);
+		yProj = yProj - (.0000013 * yProj);
 		xProj = ((xProj * Math.pow(2, zoom)) - (mapWidth / 2)) / Math.pow(2, zoom);
 		yProj = ((yProj * Math.pow(2, zoom)) - (mapHeight / 2)) / Math.pow(2, zoom); 
 		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
@@ -48,8 +58,8 @@ public class GMaps {
 	
 	public static LatLng getBottomLeftBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
 		LatLng point = new LatLng(0, 0);
-		xProj = xProj - (.000011 * xProj);
-		yProj = yProj - (.0000045 * yProj);
+		xProj = xProj - (.000016 * xProj);
+		yProj = yProj + (.0000016 * yProj);
 		xProj = ((xProj * Math.pow(2, zoom)) - (mapWidth / 2)) / Math.pow(2, zoom);
 		yProj = ((yProj * Math.pow(2, zoom)) + (mapHeight / 2)) / Math.pow(2, zoom); 
 		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
@@ -58,8 +68,8 @@ public class GMaps {
 	
 	public static LatLng getBottomRightBoundLatLng( double xProj, double yProj, int mapWidth, int mapHeight,int zoom) {
 		LatLng point = new LatLng(0, 0);
-		xProj = xProj + (.000011 * xProj);
-		yProj = yProj - (.0000045 * yProj);
+		xProj = xProj + (.000016 * xProj);
+		yProj = yProj + (.0000016 * yProj);
 		xProj = ((xProj * Math.pow(2, zoom)) + (mapWidth / 2)) / Math.pow(2, zoom);
 		yProj = ((yProj * Math.pow(2, zoom)) + (mapHeight / 2)) / Math.pow(2, zoom); 
 		point = convertProjToLatLng(xProj, yProj, mapWidth, mapHeight);
